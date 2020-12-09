02da343e9a1dce43f378d7277b88f78095d825a5
Yelena Strizhakova <e.strizhakova@gmail.com>
2020-12-09 11:16:00 +0500

    header markup

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,10 @@ function Header() {
     return (
     <header className="header">
         <div className="logo"></div>
+        <div className="header__auth-title-wrapper">
+            <h2 className="header__auth-title">email@mail.com</h2>
+            <h2 className="header__auth-title">Регистрация</h2>
+        </div>
     </header>
     )
 }
